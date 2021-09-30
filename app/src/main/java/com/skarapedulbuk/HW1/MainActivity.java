c6633de0be7326d03b1262eeed4f1d4127167456
skarapedulbuk <voronezh2@gmail.com>
2021-09-30 18:51:33 +0300

    1. Создать проект со следующими пользовательскими элементами: TextView, EditText, Button, Switch, CheckBox, ToggleButton. Для работы использовать LinearLayout. Использовать различные шрифты, цвета, размеры, прочие атрибуты. 2. Создать ещё один макет (если не получается, то проект) с несколькими EditText, где использовать атрибут inputType: text, textPersonName, phone, number, textPassword, textEmailAddress и другие значения. 3. Добавить в проект элемент CalendarView. 4. * Разобраться, что такое параметр ems.
    EMS - атрибут TextView и наследующих классов, ограничивающий ширину элемента. Значением является целое число (int). 1 EM эквивалентен ширине заглавной латинской буквы M текущего шрифтра текущего размера. Т.е. если android:ems="1", android:text="MMM", то на экране отобразится только одна буква M. Так же EMS перекрывается атрибутом android:layout_width="match_parent"

--- app/src/main/java/com/skarapedulbuk/HW1/MainActivity.java
+++ app/src/main/java/com/skarapedulbuk/HW1/MainActivity.java
@@ -2,7 +2,11 @@ package com.skarapedulbuk.HW1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.next_button);
+        ToggleButton calendarRotate = findViewById(R.id.rotate_calendar);
+        CalendarView calendar1 = findViewById(R.id.calendar1);
+
+        button.setOnClickListener(v -> setContentView(R.layout.activity_optional));
+
+        calendarRotate.setOnClickListener(v -> {
+            if (calendar1.getRotation() < 360) {
+                calendar1.setRotation(calendar1.getRotation() + 45);
+            } else {
+                calendar1.setRotation(0);
+            }
+        });
     }
 }
